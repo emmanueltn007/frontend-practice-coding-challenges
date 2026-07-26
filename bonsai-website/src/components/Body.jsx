f1f50e42c728a1b8f8265ccefcca5099d21c1a43
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-07-26 19:13:39 +0200

    Added the pricing plan section, styled it then added the toggling logic with JavaScript

---
+++ bonsai-website/src/components/Body.jsx
@@ -0,0 +1,11 @@
+import PricePlans from "./PricePlans";
+
+function Body () {
+    return (
+        <main>
+            <PricePlans />
+        </main>
+    );
+}
+
+export default Body
---
+++ bonsai-website/src/components/PricePlans.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function PricePlans() {
+  const [toggleButton, setToggleButton] = useState(false);
+
+  function handleToggleButton() {
+    setToggleButton((prev) => !prev);
+  }
+
+  return (
+    <section>
+      <div className="flex items-center justify-center gap-8">
+        <h2 className="text-3xl">Plans & Pricing</h2>
+
+        <div className="flex items-center gap-4">
+          <span
+            className={`font-semibold ${toggleButton === true ? "text-gray-400" : "text-gray-700"}`}
+          >
+            MONTHLY
+          </span>
+
+          <button
+            className="bg-[#00b289] px-7 py-4 rounded-3xl relative cursor-pointer"
+            onClick={() => handleToggleButton()}
+          >
+            <span
+              className={`bg-white h-6 w-6 absolute left-0 rounded-full -translate-y-1/2 top-1/2 transition-all duration-300 ease-in-out ${toggleButton === false ? "" : "left-full -translate-x-full"}`}
+            ></span>
+          </button>
+
+          <span
+            className={`font-semibold ${toggleButton === false ? "text-gray-400" : "text-gray-700"}`}
+          >
+            YEARLY
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default PricePlans;
