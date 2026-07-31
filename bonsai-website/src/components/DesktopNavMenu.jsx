b566a65ab76a19269b2d08d44716dac25ef90a1f
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-07-31 19:20:34 +0200

    Added a submenu in the Bonsai Workflow tab of the Products nav item

--- bonsai-website/src/components/DesktopNavMenu.jsx
+++ bonsai-website/src/components/DesktopNavMenu.jsx
@@ -1,3 +1,19 @@
+const productListItem = [
+    { productName: "Bonsai Workflow", description: "Look professional, win more clients and manage your business from one place" }, { productName: "Bonsai Tax", description: "Track expenses, maximize tax-writeoffs, and estimate taxes without the headache" },
+    { productName: "Bonsai Cash", description: "Manage your money with a no-fee business account" }
+];
+
+const bonsaiWorkflowSubMenu = [
+    {menuName: "Invoices", description: ""},
+    {menuName: "Proposals", description: ""},
+    {menuName: "Contracts", description: ""},
+    {menuName: "Client CRM", description: ""},
+    {menuName: "Forms & Automations", description: ""},
+    {menuName: "Time Tracking", description: ""},
+    {menuName: "Accounting", description: ""},
+    {menuName: "Task Tracking", description: ""}
+];
+
 const templateListItems = [
     {
         templateName: "Contract Templates",
@@ -38,7 +54,7 @@ function DesktopNavMenu() {
             <ul className="flex items-center gap-8">
                 {desktopNavItems.map((desktopNavItem, index) => {
                     return (
-                        <li key={index} className="group text-[#4c4d5f] flex items-center gap-2 py-2 cursor-pointer relative">
+                        <li key={index} className="group/nav-items text-[#4c4d5f] flex items-center gap-2 py-2 cursor-pointer relative">
                             {desktopNavItem}
 
                             {(desktopNavItem === "Product" || desktopNavItem === "Template") && (
@@ -75,50 +91,61 @@ export default DesktopNavMenu
 
 function ProductItem() {
     return (
-        <div className="hidden w-md bg-white group-hover:block absolute top-full z-40">
-            <div className="flex flex-col gap-2 hover:bg-gray-300 p-4">
-                <h3 className="text-2xl">
-                    Bonsai Workflow
-                </h3>
-                <p className="text-base">
-                    Look professional, win more clients and manage your business from one place
-                </p>
-            </div>
+        <div className="hidden w-md bg-white group-hover/nav-items:block absolute top-full -translate-x-1/2 z-40">
+            {productListItem.map((productItem, index) => {
+                return (
+                    <div key={index} className="group/workflow-arrow-icon group/workflow-submenu flex justify-between items-center hover:bg-gray-300 p-4 relative">
+                        <div className="flex flex-col gap-2">
+                            <h3 className="text-2xl">
+                                {productItem.productName}
+                            </h3>
+                            <p className="text-base">
+                                {productItem.description}
+                            </p>
+                        </div>
 
-            <div className="flex flex-col gap-2 hover:bg-gray-300 p-4">
-                <h3 className="text-2xl">
-                    Bonsai Tax
-                </h3>
-                <p className="text-base">
-                    Track expenses, maximize tax-writeoffs, and estimate taxes without the headache
-                </p>
-            </div>
+                        {productItem.productName === "Bonsai Workflow" && (
+                            <img className="h-6 rotate-270 hidden group-hover/workflow-arrow-icon:block" src="./images/angle-down-solid-full.svg" alt="arrow icon" />
+                        )}
 
-            <div className="flex flex-col gap-2 hover:bg-gray-300 p-4">
-                <h3 className="text-2xl">
-                    Bonsai Cash
-                </h3>
-                <p className="text-base">
-                    Manage your money with a no-fee business account
-                </p>
-            </div>
+                        {productItem.productName === "Bonsai Workflow" && (
+                            <BonsaiWorkflowSubMenu />
+                        )}
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
 
+function BonsaiWorkflowSubMenu () {
+    return (
+        <div className="bg-white hidden grid-cols-2 group-hover/workflow-submenu:grid absolute left-full w-xl z-50">
+            {bonsaiWorkflowSubMenu.map((subMenu, index) => {
+                return (
+                    <div key={index}>
+                        <span>
+                            {subMenu.menuName}
+                        </span>
+                    </div>
+                );
+            })}
         </div>
     );
 }
 
 function TemplateItem() {
     return (
-        <div className="hidden w-md bg-white group-hover:block absolute top-full z-40">
+        <div className="hidden w-md bg-white group-hover/nav-items:block absolute top-full z-40">
             <ul>
                 {templateListItems.map((listItem, index) => {
                     return (
-                        <li key={index} className="flex items-center justify-between gap-2 hover:bg-gray-300 p-4">
+                        <li key={index} className="group/template-icon flex items-center justify-between gap-2 hover:bg-gray-300 p-4">
                             <span className="flex items-center gap-2">
                                 <img src={listItem.icon} alt="template icon" />
                                 <span>{listItem.templateName}</span>
                             </span>
-                            <img className="h-6 rotate-270" src="./images/angle-down-solid-full.svg" alt="arrow icon" />
+                            <img className="hidden group-hover/template-icon:block h-6 rotate-270" src="./images/angle-down-solid-full.svg" alt="arrow icon" />
                         </li>
                     );
                 })}
